19801673759f8d9e5c592cbadd960bb1bdfaa44b
hasan <hasansennn82@gmail.com>
2023-04-25 09:37:45 +0300

    25.04

---
+++ src/test/java/tests/ApkYukleme.java
@@ -0,0 +1,33 @@
+package tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class ApkYukleme {
+        AndroidDriver<AndroidElement> driver; //android cihazlarin driveri
+        final String cihazAdi="PIXEL";
+        final String platformIsmi="Android";
+        final String version="10.0";
+        final String automation="UiAutomator2";
+        @Test
+        public void setUp() throws MalformedURLException {
+
+                DesiredCapabilities capabilities=new DesiredCapabilities();
+                capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,cihazAdi);
+                capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,platformIsmi);
+                capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
+                capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,automation);
+                capabilities.setCapability(MobileCapabilityType.APP,"C:\\PROJELER\\Appium_T-108-main\\Apps\\kiwi-com-2023-14-0.apk");
+
+                driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+                driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        }
+}
